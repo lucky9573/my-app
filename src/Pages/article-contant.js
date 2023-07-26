f8f03aec8322109c704e04e3901b3d37134d1729
likhith <kumarlikhith675@gmail.com>
2023-07-26 13:03:11 +0530

    content-created

---
+++ src/Pages/article-contant.js
@@ -0,0 +1,5 @@
+let article = [{
+    name: 'learn react',
+    title: 'Better ways to learn',
+    content: ['dslkj; kfjs;lkjoeojoiecurmopio irpoiv irjfgpoiu[0joirdupcjlkmfg kghoreokjhlfkjhdpiocm mx fj jnh,o;cnl nknk otg nmnv.mfdrkhoele kj;lkdh f kdngs;heroc,jrgms kjrlk;hckgioupodjn,trmvmcn  rtdgfnk;mrh;kjg']
+}]
