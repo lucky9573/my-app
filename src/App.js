9038a8d3b24761792a78a3038cb7bbedad5fdf87
likhith <kumarlikhith675@gmail.com>
2023-07-26 08:31:45 +0530

    writing markup in jsx

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function MyButton() {
 
 export default function MyApp() {
     return (
-        <div>
-            <h1>Welcome to my app</h1>
-            <MyButton />
-        </div>
+        <>
+            <h1>About</h1>
+            <p>Hello there.<br />How do you do?</p>
+        </>
     );
 }
