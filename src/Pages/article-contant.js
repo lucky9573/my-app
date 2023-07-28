22687f7b11f2cf3099a447099689b4fef15947fa
likhith <kumarlikhith675@gmail.com>
2023-07-28 06:26:08 +0530

    Css added

--- src/Pages/article-contant.js
+++ src/Pages/article-contant.js
@@ -2,4 +2,15 @@ let article = [{
     name: 'learn react',
     title: 'Better ways to learn',
     content: ['dslkj; kfjs;lkjoeojoiecurmopio irpoiv irjfgpoiu[0joirdupcjlkmfg kghoreokjhlfkjhdpiocm mx fj jnh,o;cnl nknk otg nmnv.mfdrkhoele kj;lkdh f kdngs;heroc,jrgms kjrlk;hckgioupodjn,trmvmcn  rtdgfnk;mrh;kjg']
-}]
+},
+{
+    name: 'learn Nodejs',
+    title: 'Better ways to learn',
+    content: ['dslkj; kfjs;lkjoeojoiecurmopio irpoiv irjfgpoiu[0joirdupcjlkmfg kghoreokjhlfkjhdpiocm mx fj jnh,o;cnl nknk otg nmnv.mfdrkhoele kj;lkdh f kdngs;heroc,jrgms kjrlk;hckgioupodjn,trmvmcn  rtdgfnk;mrh;kjg']
+},
+{
+    name: 'learn Express js',
+    title: 'Better ways to learn',
+    content: ['dslkj; kfjs;lkjoeojoiecurmopio irpoiv irjfgpoiu[0joirdupcjlkmfg kghoreokjhlfkjhdpiocm mx fj jnh,o;cnl nknk otg nmnv.mfdrkhoele kj;lkdh f kdngs;heroc,jrgms kjrlk;hckgioupodjn,trmvmcn  rtdgfnk;mrh;kjg']
+}
+]
